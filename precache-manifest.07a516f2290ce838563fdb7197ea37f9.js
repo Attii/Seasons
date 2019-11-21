20d37a8be5988c4164a3feb96b4c18a3e46255f6
Attii <attij@yandex.ru>
2019-11-20 23:11:36 -0500

    Updates

---
+++ precache-manifest.07a516f2290ce838563fdb7197ea37f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3cb1d775d08e5e1979f181a69ae015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3224019dad436ea6a714",
+    "url": "/static/css/main.2bef5a67.chunk.css"
+  },
+  {
+    "revision": "e6d262759e94f9d627d5",
+    "url": "/static/js/2.941e3502.chunk.js"
+  },
+  {
+    "revision": "3224019dad436ea6a714",
+    "url": "/static/js/main.1f0f666e.chunk.js"
+  },
+  {
+    "revision": "d04afe0d2c6a88426112",
+    "url": "/static/js/runtime-main.242cf60e.js"
+  }
+]);
